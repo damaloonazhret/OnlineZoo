d97b63181c692de5bcf986210bfb0d1db7ba404d
Andrey Mironov <poiprizamoda@gmail.com>
2023-01-15 18:40:34 +0300

    btnMenu

--- app/js/main.js
+++ app/js/main.js
@@ -1,18 +1,21 @@
-// const iconMenu = document.querySelector('.menu__btn');
-// if (iconMenu) {
-// 	iconMenu.addEventListener("click", function (e) {
-// 		iconMenu.classList.toggle('active');
-// 	});
-// }
+const iconMenu = document.querySelector('.menu__btn');
+if (iconMenu) {
+	iconMenu.addEventListener("click", function (e) {
+		iconMenu.classList.toggle('active');
+	});
+}
 
 const btnMenu = document.querySelector(".menu__btn");
 const menu = document.querySelector(".header-page__box-contact");
+const background = document.querySelector(".menu__btn-overlay");
 const toggleMenu = function () {
 	menu.classList.toggle("active");
+	background.classList.toggle("active");
+	btnMenu.classList.toggle("active");
 }
 
 btnMenu.addEventListener("click", function (e) {
-	// e.stopPropagation();
+	e.stopPropagation();
 	toggleMenu();
 	btnMenu.classList.toggle("active");
 });
