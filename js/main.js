d5f4f27eaca3764ef4690fd854f887d80a12cec2
Andrey Mironov <poiprizamoda@gmail.com>
2023-01-15 20:27:59 +0300

    btnMenu-gh-pages

--- js/main.js
+++ js/main.js
@@ -1,59 +1,32 @@
-$(function () {
-
-
-
-	/* 	$('.pets-bottom').slick({
-			infinite: true,
-			prevArrow: '<img class="slider-arrows slider-arrows__left"/>',
-			nextArrow: '<img class="slider-arrows slider-arrows__rigth"/>',
-	
-			responsive: [
-				{
-					breakpoint: 1300,
-					settings: {
-						infinite: false,
-						slidesToShow: 2,
-						slidesToScroll: 1,
-						centerMode: true,
-						variableWidth: true,
-						initialSlide: 0,
-						rows: 2,
-					}
-				}
-			]
-		}); */
-
-	/* $('.video-menu__box').slick({
-		infinite: true,
-		useTransform: true,
-		useCSS: true,
-		vertical: true,
-		slidesToShow: 4,
-		nextArrow: $('.video-animals--btn'),
-		prevArrow: false,
-	}); */
-
-	
-/* 	$('.care-animals__mobile-slider').slick({
-		slidesToShow: 1,
-		nextArrow: false,
-		prevArrow: false,
-	}); */
-
-
-
-
-	/* 	$('.pets-think__box-slider').slick({
-			infinite: true,
-			slidesToShow: 2,
-			slidesToScroll: 1,
-			variableWidth: true,
-			centerMode: true,
-			rows: 2,
-			prevArrow: '<img class="slider-arrows slider-arrows__left" src="images/btn-left.svg" alt="arrow-left" />',
-			nextArrow: '<img class="slider-arrows slider-arrows__rigth" src="images/btn-right.svg" alt="arrow-rigth" />',
-		}); */
-
+const iconMenu = document.querySelector('.menu__btn');
+if (iconMenu) {
+	iconMenu.addEventListener("click", function (e) {
+		iconMenu.classList.toggle('active');
+	});
 }
 
-)
+const btnMenu = document.querySelector(".menu__btn");
+const menu = document.querySelector(".header-page__box-contact");
+const background = document.querySelector(".menu__btn-overlay");
+const toggleMenu = function () {
+	menu.classList.toggle("active");
+	background.classList.toggle("active");
+	btnMenu.classList.toggle("active");
+}
+
+btnMenu.addEventListener("click", function (e) {
+	e.stopPropagation();
+	toggleMenu();
+	btnMenu.classList.toggle("active");
+});
+
+document.addEventListener("click", function (e) {
+	const target = e.target;
+	const its_menu = target == menu || menu.contains(target);
+	const its_btnMenu = target == btnMenu;
+	const menu_is_active = menu.classList.contains("active");
+
+	if (!its_menu && !its_btnMenu && menu_is_active) {
+		toggleMenu();
+	}
+});
