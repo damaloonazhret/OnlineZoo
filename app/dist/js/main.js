a0c7b89d937c4211446ad7797dacbc544be70618
Andrey Mironov <poiprizamoda@gmail.com>
2023-02-06 22:02:28 +0300

    make popup

--- app/dist/js/main.js
+++ app/dist/js/main.js
@@ -625,9 +625,28 @@ document.addEventListener('keydown', function (e) {
 });
 
 
-const favorite = document.querySelector('.donation__popup-basic');
+const favorite = document.querySelector('.popup-link-in');
 const favoreteVisible = document.querySelector('.donation__popup-choise');
 
-favorite.addEventListener("click", function () {
-    favoreteVisible.classList.toggle('visible-popup');
+
+
+
+favorite.addEventListener("click", function (e) {
+    const popupNameIn = favorite.getAttribute('href').replace('#', '');
+    const curentPopupIn = document.getElementById(popupNameIn);
+    popupOpenIn(curentPopupIn);
+    e.preventDefault();
 });
+
+function popupOpenIn(curentPopupIn) {
+    favoreteVisible.classList.add('visible-popup');
+    favoreteVisible.addEventListener("click", function (e) {
+            if (e.target.closest('.donation__popup-choise-container')) {
+                popupCloseIn(favoreteVisible);
+            }
+        });
+}
+
+function popupCloseIn(x) {
+    favoreteVisible.classList.remove('visible-popup');
+}
