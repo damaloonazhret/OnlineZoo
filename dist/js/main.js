d40a77609d1b3eaa88dfe09d0970c55267509355
Andrey Mironov <poiprizamoda@gmail.com>
2023-02-12 00:21:10 +0300

    input's work

--- dist/js/main.js
+++ dist/js/main.js
@@ -534,13 +534,176 @@ function slideList () {
 // arrowRightReviev.addEventListener('click', onButtonClickReview('right'));
 // arrowLeftReview.addEventListener('click', onButtonClickReview('left'));
 // sliderStrip.addEventListener('transitionend', onSlidesBoxTransitionEndReview);
+const openPanels = document.querySelectorAll('.popup-link-not-close');
+const menuPanel = document.querySelectorAll('.donation__popup-choise-content');
+const overlay = document.querySelectorAll('.menu__list-overlay');
+const btnMenuPanel = document.querySelectorAll('.donation__popup-input');
+const popups = document.querySelectorAll('.donation__popup-choise');
+const arrowBox = document.querySelectorAll('.donation__popup-input-container-box');
+const buttonDollars = document.querySelectorAll('.donation__popup-price input');
+const clearInput = document.querySelector('.donation__popup-clear-block');
+const donationInformationSelect = document.querySelector('#donation_information');
+const donationInformationSelectTwo = document.querySelector('#donation_information_2');
+const donationInformationSelectThree = document.querySelector('#donation_information_3');
+const donationInformationInput = donationInformationSelect.querySelector('input[data-input]');
+const donationInformationInputTwo = donationInformationSelectTwo.querySelector('input[data-input]');
+const donationInformationInputThree = donationInformationSelectThree.querySelector('input[data-input]');
+
+buttonDollars.forEach(el => {
+    el.addEventListener('click', function () {
+        if (!el.classList.contains('active')) {
+            searchButton();
+            el.classList.add('active');
+            el.setAttribute('name', 'donationAmount');
+        }
+    });
+});
+
+function searchButton() {
+    buttonDollars.forEach(el => {
+        if (el.classList.contains('active')) {
+            el.classList.remove('active');
+            el.removeAttribute('name');
+            clearInput.removeAttribute('name');
+        }
+    });
+}
+
+clearInput.addEventListener('click', function () {
+    clearInput.setAttribute('name', 'donationAmount');
+    searchButton();
+});
+
+
+const selectItem = (popup, input) => {
+    popup.addEventListener('click', (e) => {
+        const target = e.target;
+        if (target.tagName === 'P') {
+            input.value = target.textContent;
+            formData.fullName = target.textContent;
+        }
+    });
+};
+selectItem(donationInformationSelect, donationInformationInput);
+selectItem(donationInformationSelectTwo, donationInformationInputTwo);
+selectItem(donationInformationSelectThree, donationInformationInputThree);
+
+
+function serializeForm(formNode) {
+    console.log(formNode.elements);
+}
+
+function handleFormSubmit(event) {
+    event.preventDefault();
+    for (let i = 0; i < applicantForms.length; i++) {
+        const el = applicantForms[i];
+        serializeForm(el);
+    }
+
+}
+
+const applicantForms = document.querySelectorAll('.save-data');
+
+for (let i = 0; i < applicantForms.length; i++) {
+    const applicantForm = applicantForms[i];
+    applicantForm.addEventListener('submit', function (e) {
+        handleFormSubmit(e);
+    });
+}
+
+function serializeForm(formNode) {
+    const { elements } = formNode;
+
+    const data = Array.from(elements)
+        .map((element) => {
+            const { name, type } = element;
+            const value = type === 'checkbox' ? element.checked : element.value
+
+            return { name, value };
+        })
+        .filter((item) => !!item.name);
+    const obj = Object.assign({}, data);
+    console.log(obj);
+}
+
+function serializeForm(formNode) {
+    const { elements } = formNode;
+
+    Array.from(elements)
+        .map((element) => {
+            const { name, type } = element;
+            const value = type === 'checkbox' ? element.checked : element.value
+            formData[name] = value;
+            return { name, value };
+        })
+        .filter((item) => !!item.name);
+    console.log(formData);
+}
+// function serializeForm(formNode) {
+//     const { elements } = formNode;
+
+//     Array.from(elements)
+//         .forEach((element) => {
+//             const { name, value } = element;
+//             formData[name] = value;
+//         });
+//     console.log(formData);
+// }
+
+const formData = {
+    donationAmount: '',
+    specialPet: '',
+    isGift: false,
+    fullName: '',
+    email: '',
+    cardNumber: '',
+    cvv: '',
+    expMonth: '',
+    expYear: '',
+};
+
+
+for (let i = 0; i < openPanels.length; i++) {
+    const openPanelActive = openPanels[i],
+        overlayActive = overlay[i],
+        arrowBoxActive = arrowBox[i];
+
+    openPanelActive.addEventListener("click", function (e) {
+        const openPanelName = openPanelActive.getAttribute('href').replace('#', ''),
+            curentopenPanel = document.getElementById(openPanelName);
+
+        openPanel(curentopenPanel, overlayActive, arrowBoxActive);
+        e.preventDefault();
+    });
+}
+
+function openPanel(curentopenPanel, overlayActive, arrowBoxActive) {
+    curentopenPanel.classList.add('open');
+    overlayActive.classList.add('active');
+    arrowBoxActive.classList.add('active');
+    overlayActive.addEventListener('click', function () {
+        curentopenPanel.classList.remove('open');
+        overlayActive.classList.remove('active');
+        arrowBoxActive.classList.remove('active');
+    });
+    curentopenPanel.addEventListener('click', function () {
+        curentopenPanel.classList.remove('open');
+        overlayActive.classList.remove('active');
+        arrowBoxActive.classList.remove('active');
+    });
+}
+
+
+
+
+
 const popupLinks = document.querySelectorAll('.popup-link');
 const body = document.querySelector('html');
 const lockPadding = document.querySelectorAll('.lock-padding');
 
 let unlock = true;
 
-const timeout = 800;
+const timeout = 400;
 
 
 if (popupLinks.length > 0) {
@@ -550,7 +713,6 @@ if (popupLinks.length > 0) {
             const popupName = popupLink.getAttribute('href').replace('#', '');
             const curentPopup = document.getElementById(popupName);
             popupOpen(curentPopup);
-            e.preventDefault();
         });
     }
 }
@@ -595,10 +757,10 @@ function bodyLock() {
         body.style.paddingRight = lockPaddingValue;
         body.classList.add('lock');
 
-            unlock = false;
-            setTimeout(function () {
-                unlock = true;
-            }, timeout);
+        unlock = false;
+        setTimeout(function () {
+            unlock = true;
+        }, timeout);
 
     }
 }
@@ -625,130 +787,4 @@ document.addEventListener('keydown', function (e) {
         popupClose(popupActive);
     }
 });
-
-
-const openPanels = document.querySelectorAll('.popup-link-not-close');
-const menuPanel = document.querySelectorAll('.donation__popup-choise-content');
-const overlay = document.querySelectorAll('.menu__list-overlay');
-const btnMenuPanel = document.querySelectorAll('.donation__popup-input');
-const popups = document.querySelectorAll('.donation__popup-choise');
-const arrowBox = document.querySelectorAll('.donation__popup-input-container-box');
-
-const donationInformationPopup = document.querySelector('#donation_information');
-const donationInformationPopupTwo = document.querySelector('#donation_information_2');
-const donationInformationPopupThree = document.querySelector('#donation_information_3');
-const donationInformationInput = donationInformationPopup.querySelector('input[data-input]');
-const donationInformationInputTwo = donationInformationPopupThree.querySelector('input[data-input]');
-const donationInformationInputThree = donationInformationPopupThree.querySelector('input[data-input]');
-
-const select = (popup, input) => {
-    popup.addEventListener('click', (e) => {
-        const target = e.target;
-        if (target.tagName === 'BUTTON') {
-            input.value = target.textContent;
-        }
-    });
-};
-
-select(donationInformationPopup, donationInformationInput);
-select(donationInformationPopupTwo, donationInformationInputTwo);
-select(donationInformationPopupThree, donationInformationInputThree);
-
-
-// for (let i = 0; i < popups.length; i++) {
-//   for (let i = 0; i < btnMenuPanel.length; i++) {
-//     popups[i].addEventListener("click", function (e) {
-//       const target = e.target;
-//       const tex = target.firstChild.textContent.trim();
-//       const aliment = btnMenuPanel[i];
-//       // setPlaceholder(tex, aliment);
-//       aliment.placeholder = tex;
-//     });
-//   }
-// }
-// popups.forEach(elem => {
-//     btnMenuPanel.forEach(element => {
-//     elem.addEventListener("click", function (e) {
-//         const target = e.target;
-//         const tex = target.firstChild.textContent.trim();
-//         const aliment = element;
-//             setPlaceholder(tex, aliment);
-//             e.preventDefault();
-//         });
-//     });
-// });
-
-function setPlaceholder(tex, aliment) {
-    aliment.placeholder = tex;
-}
-
-
-for (let i = 0; i < openPanels.length; i++) {
-    const openPanelActive = openPanels[i],
-            overlayActive = overlay[i],
-            arrowBoxActive = arrowBox[i];
-            
-    openPanelActive.addEventListener("click", function (e) {
-        const openPanelName = openPanelActive.getAttribute('href').replace('#', ''),
-                curentopenPanel = document.getElementById(openPanelName);
-
-        openPanel(curentopenPanel, overlayActive, arrowBoxActive);
-        e.preventDefault();
-    });
-}
-
-function openPanel(curentopenPanel, overlayActive, arrowBoxActive) {
-    curentopenPanel.classList.add('open');
-    overlayActive.classList.add('active');
-    arrowBoxActive.classList.add('active');
-    overlayActive.addEventListener('click', function () {
-        curentopenPanel.classList.remove('open');
-        overlayActive.classList.remove('active');
-        arrowBoxActive.classList.remove('active');
-    });
-    curentopenPanel.addEventListener('click', function () {
-        curentopenPanel.classList.remove('open');
-        overlayActive.classList.remove('active');
-        arrowBoxActive.classList.remove('active');
-    });
-}
-
-// if (openPanels.length > 0) {
-//     for (let index = 0; index < openPanels.length; index++) {
-//         const openPanel = openPanels[index];
-//         openPanel.addEventListener("click", function (e) {
-//             const openPanelName = openPanel.getAttribute('href').replace('#', '');
-//             const curentopenPanel = document.getElementById(openPanelName);
-//             OpenPanele(curentopenPanel);
-//             e.preventDefault();
-//         });
-//     }
-// }
-
-// function OpenPanele(curentopenPanel) {
-//     if (curentopenPanel) {
-//         curentopenPanel.classList.add('open');
-//         menuList.classList.add("active");
-//         curentopenPanel.addEventListener("click", function (e) {
-//             if (!e.target.closest('.donation__popup-choise')) {
-//                 ClosePanele(e.target.closest('.donation__popup-choise-content'));
-//             }
-//         });
-//     }
-// }
-
-// function ClosePanele() {
-//     menuPanel.classList.remove('open');
-//     menuList.classList.remove("active");
-// }
-
-// document.addEventListener("click", function (e) {
-//     const target = e.target;
-//     const itsMenu = target == menuPanel || menuPanel.contains(target);
-//     const itsBtnMenu = target == btnMenuPanel;
-//     const menuIsActive = menuPanel.classList.contains("open");
-//     if (!itsMenu && !itsBtnMenu && menuIsActive) {
-//         ClosePanele();
-//     }
-// });
 
